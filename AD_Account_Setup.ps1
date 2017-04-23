a88a4d8fe5bc3b108ca3ee1a16f9926204eff3b4
Frantz Muench <Frantz.Muench@wcbay.com>
2017-04-23 07:38:20 -0400

    Framework

--- AD_Account_Setup.ps1
+++ AD_Account_Setup.ps1
@@ -1,1 +1,26 @@
+﻿$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
+$VariableHere = Read-Host -Prompt ''
 
+New-ADUser -
+
+
+
+New-ADUser [-Name] <string> [-AccountExpirationDate <System.Nullable[System.DateTime]>] [-AccountNotDelegated <System.Nullable[bool]>] [-AccountPassword <SecureString>] [-AllowReversiblePasswordEncryption <System.Nullable[bool]>] [-AuthType {Negotiate | Basic}] [-CannotChangePassword <System.Nullable[bool]>] [-Certificates <X509Certificate[]>] [-ChangePasswordAtLogon <System.Nullable[bool]>] [-City <string>] [-Company <string>] [-Country <string>] [-Credential <PSCredential>] [-Department <string>] [-Description <string>] [-DisplayName <string>] [-Division <string>] [-EmailAddress <string>] [-EmployeeID <string>] [-EmployeeNumber <string>] [-Enabled <System.Nullable[bool]>] [-Fax <string>] [-GivenName <string>] [-HomeDirectory <string>] [-HomeDrive <string>] [-HomePage <string>] [-HomePhone <string>] [-Initials <string>] [-Instance <ADUser>] [-LogonWorkstations <string>] [-Manager <ADUser>] [-MobilePhone <string>] [-Office <string>] [-OfficePhone <string>] [-Organization <string>] [-OtherAttributes <hashtable>] [-OtherName <string>] [-PassThru <switch>] [-PasswordNeverExpires <System.Nullable[bool]>] [-PasswordNotRequired <System.Nullable[bool]>] [-Path <string>] [-POBox <string>] [-PostalCode <string>] [-ProfilePath <string>] [-SamAccountName <string>] [-ScriptPath <string>] [-Server <string>] [-ServicePrincipalNames <string[]>] [-SmartcardLogonRequired <System.Nullable[bool]>] [-State <string>] [-StreetAddress <string>] [-Surname <string>] [-Title <string>] [-TrustedForDelegation <System.Nullable[bool]>] [-Type <string>] [-UserPrincipalName <string>] [-Confirm] [-WhatIf] [<CommonParameters>]
